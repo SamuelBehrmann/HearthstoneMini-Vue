5485e8568f89239b5bc714055dc074d437acc7f7
= <=>
2023-12-09 11:07:17 +0100

    feat: WelcomeView, RulesView, Meta

--- src/router/index.js
+++ src/router/index.js
@@ -5,19 +5,36 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
+      component: HomeView,
+      meta: {
+        title: 'Home',
+      },
       name: 'home',
-      component: HomeView
+      path: '/',
     },
     {
-      path: '/about',
-      name: 'about',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: () => import('../views/GameView.vue'),
+      meta: {
+        title: 'Game',
+      },
+      name: 'game',
+      path: '/game',
+    },
+    {
+      component: () => import('../views/RulesView.vue'),
+      meta: {
+        title: 'Rules',
+      },
+      name: 'rules',
+      path: '/rules',
     }
   ]
 })
 
+router.beforeEach((to, from) => {
+  document.title = to.meta?.title ?? 'Default Title'
+})
 export default router
