3e6fd17dcdd6a0b62d0da0370cee02c14e299ce7
= <=>
2023-12-14 14:43:48 +0100

    feat: game not rendered properly

--- src/router/index.js
+++ src/router/index.js
@@ -23,7 +23,7 @@ const router = createRouter({
         title: 'Game'
       },
       name: 'game',
-      path: '/game'
+      path: '/game',
     },
     {
       component: () => import('../views/GameSettings.vue'),
