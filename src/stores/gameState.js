c3044303777afa100583d284f948523b84cf3d69
= <=>
2023-12-14 14:55:23 +0100

    feat: auto reconnect

--- src/stores/gameState.js
+++ src/stores/gameState.js
@@ -7,37 +7,44 @@ export const gameState = defineStore("gameState", () => {
     var connected = ref(false);
     var data = ref({});
 
-    const websocket = new WebSocket(CONSTANTS.websocketUrl);
-    websocket.setTimeout;
-
-    websocket.onopen = function () {
-        console.log("Connected to Websocket");
-        axios.get(CONSTANTS.endpoint + "/game").then((response) => {
-            data.value = response.data;
-            connected.value = true;
-        });
-    }
-
-    websocket.onclose = function () {
-        console.log('Connection with Websocket Closed!');
-        connected.value = false;
-    };
-
-    websocket.onerror = function (error) {
-        console.log('Error in Websocket Occured: ' + error);
-    };
-
-    websocket.onmessage = function (msg) {
-        if (typeof msg.data === "string") {
-            const newData = JSON.parse(msg.data);
-            data.value = newData;
-        }
-    };
+    const websocket = connectWebsocket();
 
     return {
         connected,
         data,
         websocket,
     };
+
+    function connectWebsocket() {
+        const websocket = new WebSocket(CONSTANTS.websocketUrl);
+        websocket.setTimeout;
+
+        websocket.onopen = function () {
+            console.log("Connected to Websocket");
+            axios.get(CONSTANTS.endpoint + "/game").then((response) => {
+                data.value = response.data;
+                connected.value = true;
+            });
+        };
+
+        websocket.onclose = function () {
+            console.log('Connection with Websocket Closed!');
+            connected.value = false;
+            setTimeout(connectWebsocket, 5000);
+            connectWebsocket();
+        };
+
+        websocket.onerror = function (error) {
+            console.log('Error in Websocket Occured: ' + error);
+        };
+
+        websocket.onmessage = function (msg) {
+            if (typeof msg.data === "string") {
+                const newData = JSON.parse(msg.data);
+                data.value = newData;
+            }
+        };
+        return websocket;
+    }
 });
 
